7345549613db49ee910630e693eb72fb6600771d
Vishesh Jain <visheshjain003@icloud.com>
2025-04-05 04:43:52 +0530

    Create whoispaying.js

---
+++ whoispaying.js
@@ -0,0 +1,8 @@
+function whosPaying(names) {
+    
+    //Write your code here.
+    var n = Math.random();
+    n = Math.round((n * names.length))
+    return names[n]+" is going to buy lunch today!"
+  
+}
